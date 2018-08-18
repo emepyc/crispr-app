c02eeffda8e7e91b228a15a9a933cd2ff97d2f81
Miguel Pignatelli <emepyc@gmail.com>
2018-08-18 17:07:16 +0200

    better responsive design for logo

--- src/containers/depmapGenesHeader/index.js
+++ src/containers/depmapGenesHeader/index.js
@@ -4,17 +4,23 @@ import React from 'react';
 import Logo from '../../assets/projectScore.png';
 import './depmapGenesHeader.css';
 
-const header = () => (
-  <Jumbotron className="text-center mb-3">
-    <Container>
-      <img src={Logo} width={'500px'} />
-      <p className="lead">Genetic screens to identify cancer dependencies</p>
-      <p className="my-2" style={{ textAlign: 'center' }}>
-        Project Score uses CRISPR-Cas9 whole genome drop out screening
-        technology to identify novel oncology drug targets.
-      </p>
-    </Container>
-  </Jumbotron>
-);
+const header = () => {
+  return (
+    <Jumbotron className="text-center mb-3">
+      <Container>
+        <Row>
+          <Col md={{ size: 8, offset: 2 }} xs={12}>
+            <img src={Logo} style={{ maxWidth: '100%', height: 'auto' }} />
+          </Col>
+        </Row>
+        <p className="lead">Genetic screens to identify cancer dependencies</p>
+        <p className="my-2" style={{ textAlign: 'center' }}>
+          Project Score uses CRISPR-Cas9 whole genome drop out screening
+          technology to identify novel oncology drug targets.
+        </p>
+      </Container>
+    </Jumbotron>
+  );
+};
 
 export default header;
